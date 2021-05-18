a124be353afab61098e42928e81906af3ab63728
Josephh Carita <joseph.carita@tecsup.edu.pe>
2021-05-18 08:12:50 -0700

    Initial Commit

---
+++ ColeccionDJuegosII/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColeccionDJuegosII
+//
+//  Created by Joseph Antonio on 5/18/21.
+//  Copyright © 2021 Joseph Antonio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
